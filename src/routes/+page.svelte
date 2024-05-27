d291024f9fac7adab0cdd7e512e58fb8fbed2a0a
Micha Mailaender <micha@mailaender.me>
2024-05-27 18:14:44 +0200

    Typo and responsivenes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -43,67 +43,73 @@
 	];
 </script>
 
-<div class="mx-64 flex max-w-[1512px] flex-col pt-8">
-	<div class="flex flex-row justify-between">
-		<div class="flex flex-row items-center gap-3">
-			<Avatar src={'/favicon.png'} alt="skeleton" size="w-10" background="bg-transparent" />
-			<div class="flex flex-col">
-				<p class="text-sm font-extrabold text-surface-950-50">{m.name()}</p>
-				<p class="text-sm text-surface-600-400">{m.title()}</p>
+<div class="container mx-auto">
+	<div class="mx-6 flex max-w-[1280px] flex-col pt-8 md:mx-12 xl:mx-64">
+		<div class="flex w-full flex-col justify-between md:flex-row">
+			<div class="flex w-full flex-row items-center gap-3 pb-8 md:pb-0">
+				<Avatar src={'/favicon.png'} alt="skeleton" size="w-10" background="bg-transparent" />
+				<div class="flex flex-col">
+					<p class="text-sm font-extrabold text-surface-950-50">{m.name()}</p>
+					<p class="text-sm text-surface-600-400">{m.title()}</p>
+				</div>
 			</div>
+			<Files />
 		</div>
-		<Files />
-	</div>
 
-	<div class="grid grid-cols-12 pt-40">
-		<div class="col-span-5 col-start-1 flex flex-col gap-6">
-			<h1 class="h1 leading-tight tracking-tight text-surface-950-50">
-				{m.description_1()}
-			</h1>
-			<p class="text-lg text-surface-600-400">
-				{m.description_2()}
-			</p>
-		</div>
-		<div class="col-span-6 col-start-7 pl-10">
-			<div class="w-full">
-				<div class="relative" style="padding-top: 56.25%;">
-					<iframe
-						class="absolute left-0 top-0 h-full w-full"
-						src="https://www.youtube.com/embed/uNSXTxPHHXU?rel=0"
-						title="YouTube video player"
-						frameborder="0"
-						allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-						allowfullscreen
-					>
-					</iframe>
+		<div class="flex flex-col pt-24 md:pt-40 lg:grid lg:grid-cols-12">
+			<div class="col-span-5 col-start-1 flex flex-col gap-6">
+				<h1 class="h1 leading-tight tracking-tight text-surface-950-50">
+					{m.description_1()}
+				</h1>
+				<p class="text-lg text-surface-600-400">
+					{m.description_2()}
+				</p>
+			</div>
+			<div class="col-span-6 col-start-7 pt-10 lg:pl-10 lg:pt-0">
+				<div class="w-full">
+					<div class="relative" style="padding-top: 56.25%;">
+						<iframe
+							class="absolute left-0 top-0 h-full w-full overflow-hidden rounded-xl"
+							src="https://www.youtube.com/embed/uNSXTxPHHXU?rel=0"
+							title="YouTube video player"
+							frameborder="0"
+							allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+							allowfullscreen
+						>
+						</iframe>
+					</div>
 				</div>
 			</div>
 		</div>
-	</div>
 
-	<h2 class="h3 border-b pb-16 pt-64 border-surface-950-50">{m.experience()}</h2>
+		<h2 class="h3 border-b pb-16 pt-32 border-surface-950-50 lg:pt-64">{m.experience()}</h2>
 
-	<div class=" flex flex-col pb-40">
-		{#each experiences as experience}
-			<div class=" grid grid-cols-12 gap-6 border-b py-6 border-surface-200-800">
-				<div class="col-span-3 col-start-1 text-base font-bold">{experience.title}</div>
-				<div class="col-span-6 col-start-4 flex flex-col gap-2">
-					{#if experience.company}
-						<span class="text-base font-bold">{experience.company}</span>
-					{/if}
+		<div class=" flex flex-col pb-40">
+			{#each experiences as experience}
+				<div
+					class=" flex flex-col gap-6 border-b py-6 border-surface-200-800 md:grid md:grid-cols-12"
+				>
+					<div class="col-span-3 col-start-1 text-xl font-bold md:text-base">
+						{experience.title}
+					</div>
+					<div class="col-span-6 col-start-4 flex flex-col gap-2">
+						{#if experience.company}
+							<span class="text-base font-bold">{experience.company}</span>
+						{/if}
 
-					{#each experience.description as desc}
-						<p class=" text-base text-surface-600-400 {desc.startsWith('“') ? 'italic' : ''}">
-							{desc}
+						{#each experience.description as desc}
+							<p class=" text-base text-surface-600-400 {desc.startsWith('“') ? 'italic' : ''}">
+								{desc}
+							</p>
+						{/each}
+					</div>
+					<div class="col-span-2 col-start-11">
+						<p class="w-full text-base text-surface-600-400 md:text-right">
+							{experience.date}
 						</p>
-					{/each}
+					</div>
 				</div>
-				<div class="col-span-2 col-start-11">
-					<p class="w-full text-right text-base text-surface-600-400">
-						{experience.date}
-					</p>
-				</div>
-			</div>
-		{/each}
+			{/each}
+		</div>
 	</div>
 </div>
--- src/routes/pdf-files.svelte
+++ src/routes/pdf-files.svelte
@@ -5,10 +5,10 @@
 	import * as m from '$lib/paraglide/messages.js';
 </script>
 
-<div class="flex flex-row gap-2">
+<div class="flex w-full flex-row gap-2 md:justify-end">
 	<Dialog.Root>
 		<Dialog.Trigger
-			class="easy-in rounded-full border px-4 text-sm transition-all duration-200 text-surface-950-50 border-surface-200-800 hover:border-surface-300-700"
+			class="easy-in h-10 rounded-full border px-4 text-sm transition-all duration-200 text-surface-950-50 border-surface-200-800 hover:border-surface-300-700"
 		>
 			{m.letterOfRecommendation()}
 		</Dialog.Trigger>
@@ -16,26 +16,33 @@
 			<Dialog.Overlay
 				transition={fade}
 				transitionConfig={{ duration: 300 }}
-				class="fixed inset-0 z-50 bg-black/80"
+				class="fixed inset-0 z-50 bg-surface-950/80"
 			/>
 			<Dialog.Content
-				class="rounded-card-lg bg-background shadow-popover fixed left-[50%] top-[50%] z-50 h-full w-full max-w-[1000px] translate-x-[-50%] translate-y-[-50%] border p-5 outline-none md:max-h-[94%]"
+				class="rounded-card-lg bg-background shadow-popover fixed left-[50%] top-[50%] z-50 h-full w-full max-w-[1000px] translate-x-[-50%] translate-y-[-50%] p-5 outline-none md:max-h-[94%]"
 			>
-				<Dialog.Title class="text-surface-50-950">{m.letterOfRecommendation()}</Dialog.Title>
+				<Dialog.Title class="text-2xl font-bold text-surface-50-950"
+					>{m.letterOfRecommendation()}</Dialog.Title
+				>
 				<Separator.Root class="bg-muted -mx-5 mb-6 mt-5 block h-px" />
+
 				<iframe
-					class="h-[94%]"
+					class="h-[94%] overflow-hidden rounded-xl"
 					title="Empfehlungsschreiben"
 					src="/Empfehlungsschreiben_Eden-Academy_Micha-Mailaender.pdf"
 					width="100%"
 					style="border: none;"
 				>
 				</iframe>
+
 				<Dialog.Close
 					class="focus-visible:ring-foreground focus-visible:ring-offset-background active:scale-98 absolute right-5 top-5 rounded-md focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2"
 				>
 					<div>
-						<X class="text-foreground size-5" />
+						<button type="button" class="btn-icon preset-filled-surface-50-950"
+							><X class="text-foreground " /></button
+						>
+
 						<span class="sr-only">Close</span>
 					</div>
 				</Dialog.Close>
@@ -45,7 +52,7 @@
 
 	<Dialog.Root>
 		<Dialog.Trigger
-			class="easy-in rounded-full border px-4 text-sm transition-all duration-200 text-surface-950-50 hover:border-surface-200-800"
+			class="easy-in h-10 rounded-full border px-4 text-sm transition-all duration-200 text-surface-950-50 border-surface-200-800 hover:border-surface-300-700"
 		>
 			{m.jobReference()}
 		</Dialog.Trigger>
@@ -56,15 +63,14 @@
 				class="fixed inset-0 z-50 bg-black/80"
 			/>
 			<Dialog.Content
-				class="rounded-card-lg bg-background shadow-popover fixed left-[50%] top-[50%] z-50 h-full w-full max-w-[1000px] translate-x-[-50%] translate-y-[-50%] border p-5 outline-none md:max-h-[94%]"
+				class="rounded-card-lg bg-background shadow-popover fixed left-[50%] top-[50%] z-50 h-full w-full max-w-[1000px] translate-x-[-50%] translate-y-[-50%] p-5 outline-none md:max-h-[94%]"
 			>
-				<Dialog.Title
-					class="flex w-full items-center justify-center text-lg font-semibold tracking-tight"
+				<Dialog.Title class="text-2xl font-bold text-surface-50-950"
 					>{m.jobReference()} doubleSlash GmbH</Dialog.Title
 				>
 				<Separator.Root class="bg-muted -mx-5 mb-6 mt-5 block h-px" />
 				<iframe
-					class="h-[94%]"
+					class="h-[94%] overflow-hidden rounded-xl"
 					title="Job reference doubleSlash GmbH"
 					src="/Arbeitszeugnis_doubleSlash_MichaMailaender.pdf"
 					width="100%"
@@ -75,7 +81,10 @@
 					class="focus-visible:ring-foreground focus-visible:ring-offset-background active:scale-98 absolute right-5 top-5 rounded-md focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2"
 				>
 					<div>
-						<X class="text-foreground size-5" />
+						<button type="button" class="btn-icon preset-filled-surface-50-950"
+							><X class="text-foreground " /></button
+						>
+
 						<span class="sr-only">Close</span>
 					</div>
 				</Dialog.Close>
