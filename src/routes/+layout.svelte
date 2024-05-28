205c0f8a90e48e9f27b71f95bd40b8e0159d0775
Micha Mailänder <micha@mailaender.me>
2024-05-28 19:32:17 +0000

    Installed vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,17 @@
 <script>
-	import { ParaglideJS } from '@inlang/paraglide-sveltekit'
-	import { i18n } from '$lib/i18n'
+	import { ParaglideJS } from '@inlang/paraglide-sveltekit';
+	import { i18n } from '$lib/i18n';
 
 	import '../app.css';
+
+	import { dev } from '$app/environment';
+	import { inject } from '@vercel/analytics';
+
+	inject({ mode: dev ? 'development' : 'production' });
 </script>
+
 <ParaglideJS {i18n}>
+	<slot></slot>
+</ParaglideJS>
 
-
-<slot></slot>
-
-
-</ParaglideJS><style></style>
+<style></style>
